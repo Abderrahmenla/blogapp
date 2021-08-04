2c76c3f67e28d6f1e2d4fa3683e8603eb9fe3e26
Abderrahmenla <abdo.vic@hotmail.com>
2021-08-04 19:18:56 +0100

    adding the article repo and the article routes

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -1,15 +1,39 @@
 <?php
 namespace App\Controller;
 
+use App\Entity\Article;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
 
 class ArticleController extends AbstractController {
   /**
-   * @Route("/",methods={"GET"})
+   * @Route("/",name="article_list",methods={"GET"})
    */
   public function index(){
-    return $this->render('articles/index.html.twig',array("name"=>"Abderrahmen"));
+    $articles=$this->getDoctrine()->getRepository(Article::class)->findAll();
+    return $this->render('articles/index.html.twig',array("articles"=>$articles));
   }
+  /**
+   * @Route("/article/{id}",name="article_show")
+   */
+  public function show($id) {
+    $article = $this->getDoctrine()->getRepository(Article::class)->find($id);
+    return $this->render('articles/show.html.twig',array('article'=>$article));
+  }
+
+  // /**
+  //  * @Route("/article/save")
+  //  */
+  // public function save(){
+  //   $entityManager = $this->getDoctrine()->getManager();
+  //   $article = new Article();
+  //   $article->setTitle('Article one');
+  //   $article->setBody('This is the body for article one');
+  //   $entityManager->persist($article);
+  //   $entityManager->flush();
+  //   return new Response('Saves an article with the id of '.$article->getId());
+  // }
+
 }
